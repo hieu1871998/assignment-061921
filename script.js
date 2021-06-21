b37cefbd2ee106a60d23f126a45a2fb2d0bd93e9
Vi Minh Hiếu <hieu1871998@gmail.com>
2021-06-21 21:30:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var studentName, nameExist = [], scienceMark, historyMark, mathMark, englishMark, geographyMark, totalMarks;
+var studentName, nameExist = [], scienceMark, historyMark, mathMark, englishMark, geographyMark, totalMarks, stdPerf;
 var labelError;
-function loadData() {
-    studentName = document.getElementById("studentName");
+function loadVariables() {
+    
     scienceMark = document.getElementById("science");
     historyMark = document.getElementById("history");
     mathMark = document.getElementById("mathematics");
@@ -9,58 +9,98 @@ function loadData() {
     geographyMark = document.getElementById("geography");
     formLabel = document.getElementsByClassName("form-label");
     labelError = document.getElementsByClassName("form-label-error");
+    loadData();
 }
-window.onload = loadData;
+function loadData() {
+    studentName = document.getElementById("studentName").value;
+    var stdN = localStorage.getItem(studentName);
+    console.log(stdN);
+    if (stdN !== null) {
+        studentName = JSON.parse(stdN).stdName;
+        scienceMark.value = JSON.parse(stdN).science;
+        historyMark.value = JSON.parse(stdN).history;
+        mathMark.value = JSON.parse(stdN).math;
+        englishMark.value = JSON.parse(stdN).english;
+        geographyMark.value = JSON.parse(stdN).geography;
+    }
+    if (JSON.parse(stdN) == null) {
+        scienceMark.value = "";
+        historyMark.value = "";
+        mathMark.value = "";
+        englishMark.value = "";
+        geographyMark.value = "";
+    }
+}
+window.onload = loadVariables, loadData;
 function submitForm() {
     totalMarks = parseInt(scienceMark.value) + parseInt(historyMark.value) + parseInt(mathMark.value) + parseInt(englishMark.value) + parseInt(geographyMark.value);
-    checkValues();   
+    checkValues();
+      
+}
+function saveData() {
+    stdPerf = {
+        stdName: studentName,
+        nameExisted: nameExist,
+        science: scienceMark.value,
+        history: historyMark.value,
+        math: mathMark.value,
+        english: englishMark.value,
+        geography: geographyMark.value,
+        total: totalMarks
+    }
+    Storage(stdPerf);
+}
+function Storage(obj) {
+    localStorage.setItem(obj.stdName,JSON.stringify(obj));
 }
 function checkValues() {
-    if (studentName.value == "") {
+    if (studentName == "") {
         showAlertBox("alert-name");
         formLabel[0].style.color = "red";
         labelError[0].style.opacity = "1";
         return false;
     }
-    if (nameExist.includes(studentName.value)) {
+    console.log(nameExist);
+    if (nameExist.includes(studentName)) {
         showAlertBox("alert-name-exist");
         formLabel[0].style.color = "red";
         labelError[0].style.opacity = "1";
         return false;
     }
-    if (scienceMark.value == "" || scienceMark.value < 0 || scienceMark.value >10) {
+    if (scienceMark.value == "" || scienceMark.value < 0 || scienceMark.value > 10) {
         showAlertBox("alert-mark");
         formLabel[1].style.color = "red";
         labelError[1].style.opacity = "1";
         return false;
     }
-    if (historyMark.value == "" || historyMark.value < 0 || scienceMark.value >10) {
+    if (historyMark.value == "" || historyMark.value < 0 || historyMark.value > 10) {
         showAlertBox("alert-mark");
         formLabel[2].style.color = "red";
         labelError[2].style.opacity = "1";
         return false;
     }
-    if (mathMark.value == "" || mathMark.value < 0 || scienceMark.value >10) {
+    if (mathMark.value == "" || mathMark.value < 0 || mathMark.value > 10) {
         showAlertBox("alert-mark");
         formLabel[3].style.color = "red";
         labelError[3].style.color = "red";
         labelError[3].style.opacity = "1";
         return false;
     }
-    if (englishMark.value == "" || englishMark.value < 0 || scienceMark.value >10) {
+    if (englishMark.value == "" || englishMark.value < 0 || englishMark.value > 10) {
         showAlertBox("alert-mark");
         formLabel[4].style.color = "red";
         labelError[4].style.opacity = "1";
         return false;
     }
-    if (geographyMark.value == "" || geographyMark.value < 0 || scienceMark.value >10) {
+    if (geographyMark.value == "" || geographyMark.value < 0 || geographyMark.value >10) {
         showAlertBox("alert-mark");
         formLabel[5].style.color = "red";
         labelError[5].style.opacity = "1";
         return false;
     }
-    nameExist.push(studentName.value);
-    resetLabel()
+    nameExist.push(studentName);
+    saveData();
+    resetLabel();
     showTable();
     insertRow();
     resetValue();
@@ -72,7 +112,7 @@ function resetLabel() {
     }    
 }
 function resetValue() {
-    studentName.value = "";
+    document.getElementById("studentName").selectedIndex = 0;
     scienceMark.value = "";
     historyMark.value = "";
     mathMark.value = "";
@@ -88,19 +128,28 @@ function insertRow() {
     var table, row, rowCount,cell, i, j;
     table = document.getElementById("table");
     rowCount = table.rows.length;
-    if (studentName.value != "") {
+    if (studentName != "") {
         row = table.insertRow(-1);
         for (i = 0; i < table.rows[0].cells.length; i++) {
             cell = row.insertCell(i);
             cell.classList.add("table-content-row", "text-color__dark");
         }
-        row.cells[0].innerHTML = studentName.value;
-        row.cells[1].innerHTML = scienceMark.value;
-        row.cells[2].innerHTML = historyMark.value;
-        row.cells[3].innerHTML = mathMark.value;
-        row.cells[4].innerHTML = englishMark.value;
-        row.cells[5].innerHTML = geographyMark.value;
-        row.cells[6].innerHTML = totalMarks;
+        row.cells[0].innerHTML = stdPerf.stdName;
+        row.cells[1].innerHTML = stdPerf.science;
+        row.cells[2].innerHTML = stdPerf.history;
+        row.cells[3].innerHTML = stdPerf.math;
+        row.cells[4].innerHTML = stdPerf.english;
+        row.cells[5].innerHTML = stdPerf.geography;
+        row.cells[6].innerHTML = stdPerf.total;
+        
+       /*
+        row.cells[0].innerHTML = studentName;
+        row.cells[1].innerHTML = scienceMark;
+        row.cells[2].innerHTML = historyMark;
+        row.cells[3].innerHTML = mathMark;
+        row.cells[4].innerHTML = englishMark;
+        row.cells[5].innerHTML = geographyMark;
+        row.cells[6].innerHTML = totalMarks;*/
     }    
 }
 var alertBox;
